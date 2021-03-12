85286a922a9624c88eeea8f4f6cf20121d8bc282
Faizan khan <mohamedfaizankhan1@gmail.com>
2021-03-12 14:56:15 +0530

    first commit && initiated bolierplate

---
+++ src/Spotify.js
@@ -0,0 +1,12 @@
+import "./Spotify.css";
+
+function App() {
+  return (
+    // BEM
+    <div className="spotify">
+      <h1>Building Spotify-clone 🚀</h1>
+    </div>
+  );
+}
+
+export default App;
