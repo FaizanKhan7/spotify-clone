6707c02e43050f47e9233a05cdaf0fdf78f09654
Faizan khan <mohamedfaizankhan1@gmail.com>
2021-03-17 22:58:51 +0530

    added songs to display on the screen

--- src/components/Body/Body.js
+++ src/components/Body/Body.js
@@ -1,7 +1,10 @@
 import React from "react";
+import "./Body.css";
 import { useStateContextValue } from "../../StateProvider";
 import Header from "../Header/Header";
-import "./Body.css";
+import { Favorite, MoreHoriz, PlayCircleFilled } from "@material-ui/icons";
+import SongRow from "../Songs/SongRow";
+
 function Body({ spotify }) {
   const [{ discover_weekly }, dispatch] = useStateContextValue();
   return (
@@ -15,6 +18,16 @@ function Body({ spotify }) {
           <h2>{discover_weekly?.name}</h2>
           <p>{discover_weekly?.description}</p>
         </div>
+      </div>
+      <div className="body__songs">
+        <div className="body__icons">
+          <PlayCircleFilled className="body__shuffle" />
+          <Favorite fontSize="large" />
+          <MoreHoriz />
+        </div>
+        {discover_weekly?.tracks.items.map((item) => (
+          <SongRow track={item.track} />
+        ))}
       </div>
     </div>
   );
--- src/reducer.js
+++ src/reducer.js
@@ -4,8 +4,8 @@ export const initialState = {
   playing: false,
   item: null,
   // just to complete the project we use the token inside this state (helps a lot while dubugging) else we just keep it null
-  token:
-    "BQAqj02L8z8a25yJSxMJzk7HIEbvvDZT1MfEP3AaSytAuVQF2VkoJsvjpmko16LhV8kH7nSRdSDtC8UnHW2rZm4jhw4kM9Ak7grur8pNRGHWl4S4NWLBM22KkgvKVtrWsyhwuIg2VdeQG6F6V-qfM-ciSZkM_9VQA6rA5TmK6EW00Tw5",
+  // token:
+  //   "BQAqj02L8z8a25yJSxMJzk7HIEbvvDZT1MfEP3AaSytAuVQF2VkoJsvjpmko16LhV8kH7nSRdSDtC8UnHW2rZm4jhw4kM9Ak7grur8pNRGHWl4S4NWLBM22KkgvKVtrWsyhwuIg2VdeQG6F6V-qfM-ciSZkM_9VQA6rA5TmK6EW00Tw5",
 };
 // the primary job of a reducer is just to listen for actions
 
